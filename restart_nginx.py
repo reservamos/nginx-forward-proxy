43397e6476ffb92a13c439f8daca8617d7288321
Guillermo García López <memogl25@gmail.com>
2022-06-09 13:51:22 -0500

    Add comments to script

--- restart_nginx.py
+++ restart_nginx.py
@@ -1,7 +1,12 @@
+#!/usr/bin/env python3
+
 import subprocess
 import re
 
+docker_image = 'reservamos/nginx-forward-proxy:latest'
 pattern = '/dev/xvda1(\s+[0-9.]+G){3}\s+(\d+)%'
+
+# Inspect hard disk usage.
 process = subprocess.Popen(['df', '-h'],
                      stdout=subprocess.PIPE,
                      stderr=subprocess.PIPE,
@@ -9,11 +14,12 @@ process = subprocess.Popen(['df', '-h'],
 stdout, stderr = process.communicate()
 
 matches = re.search(pattern, stdout)
-use_percentage = int(matches.group(2))
+hard_disk_use_percentage = int(matches.group(2))
 
-if use_percentage < 80:
+if hard_disk_use_percentage < 80:
     exit()
 
+# Get all docker container ids.
 process = subprocess.Popen(['docker', 'ps', '-q'],
                             stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE,
@@ -22,14 +28,16 @@ stdout, stderr = process.communicate()
 docker_process_ids = stdout.split('\n')[0:-1]
 
 for docker_process_id in docker_process_ids:
+    # Stop docker process with specific id.
     process = subprocess.Popen(['docker', 'stop', docker_process_id],
                                 stdout=subprocess.PIPE,
                                 stderr=subprocess.PIPE,
                                 universal_newlines=True)
     stdout, stderr = process.communicate()
-    print(stdout)
+    print(stdout, stderr)
 
-process = subprocess.Popen(['docker', 'run', '--rm', '-d', '-p', '80:3128', 'reservamos/nginx-forward-proxy:latest'],
+# Initialize nginx-forward-proxy container.
+process = subprocess.Popen(['docker', 'run', '--rm', '-d', '-p', '80:3128', docker_image],
                             stdout=subprocess.PIPE,
                             stderr=subprocess.PIPE,
                             universal_newlines=True)
