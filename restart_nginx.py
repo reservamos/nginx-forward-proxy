7a45fc50fa44542ba89a3cc25cc9c41c8c9a732d
Guillermo García López <memogl25@gmail.com>
2022-06-09 13:39:34 -0500

    Add script restart_nginx.py

---
+++ restart_nginx.py
@@ -0,0 +1,37 @@
+import subprocess
+import re
+
+pattern = '/dev/xvda1(\s+[0-9.]+G){3}\s+(\d+)%'
+process = subprocess.Popen(['df', '-h'],
+                     stdout=subprocess.PIPE,
+                     stderr=subprocess.PIPE,
+                     universal_newlines=True)
+stdout, stderr = process.communicate()
+
+matches = re.search(pattern, stdout)
+use_percentage = int(matches.group(2))
+
+if use_percentage < 80:
+    exit()
+
+process = subprocess.Popen(['docker', 'ps', '-q'],
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            universal_newlines=True)
+stdout, stderr = process.communicate()
+docker_process_ids = stdout.split('\n')[0:-1]
+
+for docker_process_id in docker_process_ids:
+    process = subprocess.Popen(['docker', 'stop', docker_process_id],
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.PIPE,
+                                universal_newlines=True)
+    stdout, stderr = process.communicate()
+    print(stdout)
+
+process = subprocess.Popen(['docker', 'run', '--rm', '-d', '-p', '80:3128', 'reservamos/nginx-forward-proxy:latest'],
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.PIPE,
+                            universal_newlines=True)
+stdout, stderr = process.communicate()
+print(stderr)
